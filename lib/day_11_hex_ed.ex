a82e282a894caaf5f83c95348ad0cc4f80adbaf0
Albert Arvidsson <albert.arvidsson@gmail.com>
2017-12-11 07:24:55 +0100

    Add Day11HexEd.steps_away/1

---
+++ lib/day_11_hex_ed.ex
@@ -0,0 +1,52 @@
+defmodule Adventofcode.Day11HexEd do
+  defstruct x: 0, y: 0, z: 0, directions: []
+
+  def steps_away(input) do
+    input
+    |> parse
+    |> process_recursively
+    |> distance_from
+  end
+
+  def process_recursively(%{directions: []} = state), do: state
+
+  def process_recursively(state) do
+    state
+    |> process
+    |> process_recursively
+  end
+
+  def process(%{directions: [direction | tail], x: x, y: y, z: z} = state) do
+    {x2, y2, z2} = travel(direction, {x, y, z})
+    %{state | x: x2, y: y2, z: z2, directions: tail}
+  end
+
+  def travel(:north, {x, y, z}), do: {x, y + 1, z - 1}
+  def travel(:north_east, {x, y, z}), do: {x + 1, y, z - 1}
+  def travel(:north_west, {x, y, z}), do: {x - 1, y + 1, z}
+  def travel(:south, {x, y, z}), do: {x, y - 1, z + 1}
+  def travel(:south_east, {x, y, z}), do: {x + 1, y - 1, z}
+  def travel(:south_west, {x, y, z}), do: {x - 1, y, z + 1}
+
+  def parse(input) do
+    directions =
+      input
+      |> String.split(",")
+      |> Enum.map(&direction_to_atom/1)
+
+    %__MODULE__{directions: directions}
+  end
+
+  defp direction_to_atom("n"), do: :north
+  defp direction_to_atom("ne"), do: :north_east
+  defp direction_to_atom("nw"), do: :north_west
+  defp direction_to_atom("s"), do: :south
+  defp direction_to_atom("se"), do: :south_east
+  defp direction_to_atom("sw"), do: :south_west
+
+  defp distance_from(current_position, other_position \\ {0, 0, 0})
+
+  defp distance_from(%{x: x1, y: y1, z: z1}, {x2, y2, z2}) do
+    Enum.max([abs(x2 - x1), abs(y2 - y1), abs(z2 - z1)])
+  end
+end
---
+++ test/day_11_hex_ed_test.exs
@@ -0,0 +1,29 @@
+defmodule Adventofcode.Day11HexEdTest do
+  use Adventofcode.FancyCase
+
+  import Adventofcode.Day11HexEd
+
+  describe "steps_away/1" do
+    test "ne,ne,ne is 3 steps away" do
+      assert 3 = "ne,ne,ne" |> steps_away()
+    end
+
+    test "ne,ne,sw,sw is 0 steps away (back where you started)" do
+      assert 0 = "ne,ne,sw,sw" |> steps_away()
+    end
+
+    test "ne,ne,s,s is 2 steps away (se,se)" do
+      assert 2 = "ne,ne,s,s" |> steps_away()
+    end
+
+    test "se,sw,se,sw,sw is 3 steps away (s,s,sw)" do
+      assert 3 = "se,sw,se,sw,sw" |> steps_away()
+    end
+
+    test "with puzzle input" do
+      with_puzzle_input("input/day_11_hex_ed.txt", fn input ->
+        assert 685 = input |> steps_away()
+      end)
+    end
+  end
+end
